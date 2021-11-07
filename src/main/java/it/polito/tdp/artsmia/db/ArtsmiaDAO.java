871e89764d895d1962a41af2af29f6e97c9880d7
marco <marco@DESKTOP-456VUAM>
2021-11-08 00:07:23 +0100

    prova commit

--- src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
+++ src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
@@ -1,41 +1,72 @@
 package it.polito.tdp.artsmia.db;
 
-import java.sql.Connection;
+import java.sql.Connection; 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.artsmia.model.ArtObject;
+import it.polito.tdp.artsmia.model.*; 
 
 public class ArtsmiaDAO {
 
-	public List<ArtObject> listObjects() {
+	public void listObjects(Map <Integer, ArtObject> map) {
 		
 		String sql = "SELECT * from objects";
-		List<ArtObject> result = new ArrayList<>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
-
-				ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
-						res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
-						res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
-						res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
 				
-				result.add(artObj);
+				if(!map.containsKey(res.getInt("object_id"))) {
+					ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
+							res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
+							res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
+							res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
+				
+					map.put(artObj.getId(), artObj);
+				}
+				
 			}
 			conn.close();
-			return result;
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return null;
 		}
 	}
+
+
+	public int getPeso(ArtObject a1, ArtObject a2) {
+		String sql = "SELECT count(*) as peso "
+				+ "FROM exhibition_objects e1, exhibition_objects e2 "
+				+ "WHERE e1.exhibition_id = e2.exhibition_id AND "
+				+ "e1.object_id = ? AND e2.object_id = ?";
+		Connection conn = DBConnect.getConnection(); 
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, a1.getId());
+			st.setInt(2, a2.getId());
+			ResultSet res = st.executeQuery(); 
+			int peso = 0; 
+			if(res.next()) {
+				peso = res.getInt("peso");
+			}
+			
+			st.close();
+			res.close();
+			conn.close();
+			return peso; 
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}}
+
 	
-}
+
--- src/main/java/it/polito/tdp/artsmia/model/Model.java
+++ src/main/java/it/polito/tdp/artsmia/model/Model.java
@@ -1,5 +1,60 @@
 package it.polito.tdp.artsmia.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.artsmia.db.ArtsmiaDAO;
+
 public class Model {
+	
+	private Graph <ArtObject, DefaultWeightedEdge> grafo; 
+	private ArtsmiaDAO dao; 
+	private Map<Integer, ArtObject> idMap; 
+	
+	public Model() {
+		dao = new ArtsmiaDAO(); 
+		idMap = new HashMap <Integer, ArtObject>();
+	}
+	
+	public void creaGrafo() {
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		// Aggiungere i vertici 
+		// 1. -> Recupero tutti gli ArtObject dal db
+		// 2. -> Li inserisco come vertici 
+		//List <ArtObject> vertici = dao.listObjects();
+		dao.listObjects(idMap);
+		Graphs.addAllVertices(grafo, idMap.values());
+		
+		// Aggiungere gli archi
+		// APPROCCIO 1
+		// -> Doppio ciclo for sui vertici
+		// -> Dati due vertici, controllo se sono collegati 
+		
+		for(ArtObject a1: this.grafo.vertexSet()) {
+			for(ArtObject a2: this.grafo.vertexSet()) {
+				if(!a1.equals(a2) && !this.grafo.containsEdge(a1, a2)) {
+					// devo collegare a1 ad a2?
+					int peso = dao.getPeso(a1, a2);
+					if(peso > 0) {
+						Graphs.addEdge(this.grafo, a1, a2, peso); 
+						
+						
+					}
+				}
+			}
+		}
+		
+		System.out.println("GRADO CREATO!");
+		System.out.println("# VERTICI: " + grafo.vertexSet().size());
+		System.out.println("# ARCHI: " + grafo.edgeSet().size());
+		
+	}
 
 }
